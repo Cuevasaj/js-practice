60be19bb63ee22842d4c2be94851ebe3b98800cc
Cuevasaj <92329439+Cuevasaj@users.noreply.github.com>
2023-01-16 12:17:38 -0700

    made a sum calculator

---
+++ sum.js
@@ -0,0 +1,13 @@
+// We need a function that that prints the sum of numbers 1-10
+
+function oneThroughTen() {
+  // starting at zero
+  let sum = 0;
+  //
+  for (let i = 1; i <= 10; i++) {
+    sum = sum + i;
+  }
+  console.log(sum);
+}
+
+oneThroughTen();
