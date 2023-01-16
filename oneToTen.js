78d584451733ea1247e4bebfb78bab3daffb2d54
Cuevasaj <92329439+Cuevasaj@users.noreply.github.com>
2023-01-16 11:20:49 -0700

    Uploading Code

---
+++ oneToTen.js
@@ -0,0 +1,16 @@
+//print numbers 1-10 : hint use a for loop; has to be a function
+
+function oneToTen() {
+  for (let i = 1; i <= 10; i++) {
+    console.log(i);
+  }
+}
+
+oneToTen();
+
+const oneTooTen = () => {
+  for (let i = 1; i <= 10; i++) {
+    console.log(i);
+  }
+};
+oneTooTen();
